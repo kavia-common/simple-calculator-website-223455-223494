40b2feb1bfbbd8554c851505f245274e621199ef
Kavia AI <production@kavia.ai>
2025-11-12 13:08:22 +0000

    Checkpoint 4 - cg2c24b1f0

--- CalculatorWebApplication/src/setupTests.js
+++ CalculatorWebApplication/src/setupTests.js
@@ -1,5 +1,8 @@
-// jest-dom adds custom jest matchers for asserting on DOM nodes.
-// allows you to do things like:
-// expect(element).toHaveTextContent(/react/i)
-// learn more: https://github.com/testing-library/jest-dom
+/**
+ * Jest setup file
+ * Loads Testing Library's jest-dom matchers so tests can use:
+ * - toBeInTheDocument
+ * - toHaveTextContent
+ * - etc.
+ */
 import '@testing-library/jest-dom';
